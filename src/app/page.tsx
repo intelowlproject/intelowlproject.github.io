b5e79447619ef1745a4f7678d7b3d39c6d7ea6ea
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-06-02 00:49:07 +0530

    testing image paths on deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
               <div></div>
               <Image
                 className="items center justify-center"
-                src="/images/nodescomplete.png"
+                src="site/images/nodescomplete.png"
                 width={290}
                 height={250}
                 alt={""}
