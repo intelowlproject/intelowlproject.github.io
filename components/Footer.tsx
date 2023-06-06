6b29bb6f3ccd130a8d68d4216f8dedb2d7454a82
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-06-07 00:32:38 +0530

    image path footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer(){
                   className="mb-6 inline-block max-w-[160px]"
                 >
                   <img
-                    src="/images/intelowlwhite.png"
+                    src="/site/images/intelowlwhite.png"
                     alt="logo"
                     className="max-w-full"
                   />
