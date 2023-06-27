50731f89524168ccd0f298fc6852e0c61039aada
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2023-06-27 10:22:42 +0200

    added testimonials and sponsors

--- constants/sponsordata.ts
+++ constants/sponsordata.ts
@@ -1,28 +1,33 @@
 export const Sponsordata = [
     {
-        companyname: "ushfhjhb",
-        companylevel: "bronze",
-        companylogo: "images/intelowlwhite.png",
-        companydesc: "akjda jlfal jfbjabj lablj abajl bajlfjl fbalj fbajlf bajlfb ajl bajlfbasljf bajlf basjlfba"
+        companyname: "Certego",
+        companylevel: "gold",
+        companylogo: "images/Certego.png",
+        companydesc: "Certego is a MDR (Managed Detection and Response) and Threat Intelligence Provider based in Italy. IntelOwl was born out of Certego's Threat intelligence R&D division and is constantly maintained and updated thanks to them."
     },
     {
-        companyname: "ushfkjbk",
-        companylevel: "bronze",
-        companylogo: "images/intelowlwhite.png",
-        companydesc: "akjda jlfal jfbjabj lablj abajl bajlfjl fbalj fbajlf bajlfb ajl bajlfbasljf bajlf basjlfba"
+        companyname: "The Honeynet Project",
+        companylevel: "gold",
+        companylogo: "images/honeynet_logo.png",
+        companydesc: "The Honeynet Project is a non-profit organization working on creating open source cyber security tools and sharing knowledge about cyber threats."
     },
     {
-        companyname: "ushfjkhkj",
-        companylevel: "bronze",
-        companylogo: "images/intelowlwhite.png",
-        companydesc: "akjda jlfal jfbjabj lablj abajl bajlfjl fbalj fbajlf bajlfb ajl bajlfbasljf bajlf basjlfba"
+        companyname: "Google Summer of Code",
+        companylevel: "gold",
+        companylogo: "images/gsoc_logo.png",
+        companydesc: "Google Summer of Code is a global, online program focused on bringing new contributors into open source software development. GSoC Contributors work with an open source organization on a 12+ week programming project under the guidance of mentors."
     },
     {
-        companyname: "ushfhgu",
-        companylevel: "bronze",
-        companylogo: "images/intelowlwhite.png",
-        companydesc: "akjda jlfal jfbjabj lablj abajl bajlfjl fbalj fbajlf bajlfb ajl bajlfbasljf bajlf basjlfba"
+        companyname: "ThreatHunter.ai",
+        companylevel: "silver",
+        companylogo: "images/threathunter_logo.png",
+        companydesc: "ThreatHunter.ai®, is a 100% Service-Disabled Veteran-Owned Small Business started in 2007 under the name Milton Security Group. ThreatHunter.ai is the global leader in Dynamic Threat Hunting. Operating a true 24x7x365 Security Operation Center with AI/ML-enhanced human Threat Hunters, ThreatHunter.ai has changed the industry in how threats are found, and mitigated in real time. For over 15 years, our teams of Threat Hunters have stopped hundreds of thousands of threats and assisted organizations in defending against threat actors around the clock."
     },
-
+       {
+        companyname: "Docker",
+        companylevel: "bronze",
+        companylogo: "images/docker_logo.png",
+        companydesc: "In 2021 IntelOwl joined the official Docker Open Source Program. This allows IntelOwl developers to easily manage Docker images and focus on writing the code."
+    },
 
 ]
--- constants/testimonialsdata.ts
+++ constants/testimonialsdata.ts
@@ -1,51 +1,36 @@
 export const TestimonialData = [
     {
        
-        name: "Lorem",
-        designation: "engineer",
-        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        name: "Kostas Tsialemis",
+        designation: "Cyber Threat Intelligence Analyst",
+        review: "As a Threat Intel Analyst, I use IntelOwl daily to expand the context of various indicators with ease by simply querying one platform instead of multiple. IntelOwl has made my workflow faster as I now have more time to concentrate on the analysis rather than collecting the data.",
 
     },
     {
        
-        name: "Lorem ipsum",
-        designation: "engineer",
-        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-
-    },    {
-       
-        name: "Lorem",
-        designation: "engineer",
-        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-
+        name: "Simone Berni",
+        designation: "Threat Intelligence Engineer",
+        review: "IntelOwl has the most welcoming community that I met in a opensource project, with weekly updates and real world use cases",
     },
     {
        
-        name: "Lorem",
-        designation: "engineer",
-        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        name: "Appaji Chintimi",
+        designation: "Security Enthusiast",
+        review: "I have been in development and also used IntelOwl, for some testing purposes. The dashboard is pretty amazing and self-explanatory. I loved the built-in dark theme. And command-line client is no smaller than that. I do think you'll be amazed by the look and feel of pyintelowl. Give both a try!",
+
+    },
+    {
+       
+        name: "KunYu Chen",
+        designation: "Founder of Quark Engine",
+        review: "IntelOwl is a one-stop solution for all to get intelligence data during threat hunting. It is easy to use with well-designed UI/UX. And it is useful since many robust analyzers are integrated into this solution. As a threat researcher and an open source lover, I highly recommend IntelOwl!",
 
     },
     {
         
-        name: "Lorem",
-        designation: "engineer",
-        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        name: "Aditya Narayan Sinha",
+        designation: "Software Developer",
+        review: "IntelOwl has a very cool and driven software team backing it up. It has built itself sustainably over GSoC participants making it closer to the OSS community.",
 
-    },
-    {
-        
-        name: "Lorem",
-        designation: "engineer",
-        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-
-    },
-    {
-        
-        name: "Lorem",
-        designation: "engineer",
-        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-
-    },
-
+    }
 ]
