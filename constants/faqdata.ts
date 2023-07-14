b02e39bb7f5b9f3b7897fc41422270419cd428b1
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2023-07-14 16:49:14 +0200

    updating content

--- constants/faqdata.ts
+++ constants/faqdata.ts
@@ -4,16 +4,16 @@ export const FAQData = [
         answer:'That is super fast and straightforward: follow the guide <a href="https://intelowl.readthedocs.io/en/latest/Installation.html" target="_blank">here</a>'
     },
     {
-        question: "I have several techinal doubts regarding that can't just be answered at Issues. Whom I should contact? ",
-        answer:'Join our <a href="https://gsoc-slack.honeynet.org/" target="_blank" >Slack community</a> where all our Development goes on.'
+        question: "I have several technical doubts regarding that can't just be answered at Issues. Whom I should contact? ",
+        answer:'Join our <a href="https://gsoc-slack.honeynet.org/" target="_blank" >Slack community</a> where all our Development goes on. Otherwise you can contact the authors via the <a href="https://twitter.com/intel_owl" target="_blank" >official IntelOwl Twitter account</a>'
     },
     {
-        question: "I don't want to use docker deployment. Are there any other ways, I could achieve this?",
-        answer:' Yes! The <a href="https://github.com/intelowlproject/IntelOwl/discussions/108" target="_blank" >discussion</a> thread is open for this. Follow it for more Updates. We do have a proper tutorial to deploy IntelOwl on Google-Kubernetes-Engine <a href="https://mostwanted002.cf/post/intel-owl-gke/" target="_blank" >here</a>.'
-    },
-    {
-        question: "I have found one API which can be included in IntelOwl. What next?",
+        question: "I have found one API / external service which can be included in IntelOwl. What next?",
         answer:'Great! Head over to <a href="https://github.com/intelowlproject/IntelOwl/issues" >GitHub Issues</a> and open a new one. Contribute to it if you wish, else just leave it there. Rest of things will be taken care of by us.'
+    },
+    {
+        question: "I would like to sponsor IntelOwl How can I do it?",
+        answer:'Great! You can go towards the <a href="https://opencollective.com/intelowl-project/donate" target="_blank">Open Collective site</a> and choose how much you would like to contribute! Even a little donation is really meaningful for us! If you are a company, please remember that the donation can be deducted from taxes because, thanks to Open Collective, we have the same legal status of a non-profit association'
     },
 ]
 
