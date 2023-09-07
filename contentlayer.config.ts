655410fc03eb58a6bf41a0728f42b57a440c6973
Syed Ahkam <smahkam57@gmail.com>
2023-09-07 17:16:26 +0530

    Fix invalid import in contentlayer config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,4 @@
-import { defineDocumentType, makeSource } from '@contentlayer/source-files'
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
