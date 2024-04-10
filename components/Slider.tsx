2a6bbbfe1a90c959b10d329326708941299ae226
Keshav Maheshwari <94895949+keshav861@users.noreply.github.com>
2024-04-10 22:17:20 +0530

    navigation arrow fixed for sponsor and testimonial. (#30)
    * Fix invalid import in contentlayer config
    
    * Update globals.css
    
    * navigation arrows fixed
    
    ---------
    
    Co-authored-by: Matteo Lodi <30625432+mlodic@users.noreply.github.com>
    Co-authored-by: Abheek Tripathy <90976669+abheektripathy@users.noreply.github.com>
    Co-authored-by: Syed Ahkam <smahkam57@gmail.com>

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -4,21 +4,24 @@
 import { TestimonialData } from "../constants/testimonialsdata";
 import { Sponsordata } from "../constants/sponsordata";
 import Link from "next/link";
-import { Autoplay } from "swiper";
+import SwiperCore, { Autoplay, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/swiper-bundle.css";
 
-interface sliderProps {
+SwiperCore.use([Autoplay, Navigation]);
+
+interface SliderProps {
   isTestimonial: boolean;
 }
 
-export default function Slider(props: sliderProps) {
+export default function Slider(props: SliderProps) {
   const { isTestimonial } = props;
 
   return (
     <>
       {isTestimonial ? (
-        <div id="testimonials-element" className="w-[100%]">
+        <div id="testimonials-element" className="w-[100%] relative">
           <div className="mx-auto max-w-screen-xl py-16 sm:px-6 lg:px-8">
             <Swiper
               spaceBetween={25}
@@ -40,49 +43,48 @@ export default function Slider(props: sliderProps) {
                 delay: 4000,
                 disableOnInteraction: false,
               }}
-              modules={[Autoplay]}
+              navigation={{
+                prevEl: ".swiper-button-prev",
+                nextEl: ".swiper-button-next",
+              }}
               className="h-full"
             >
-              {TestimonialData.map((testimonial, index) => {
-                return (
-                  <div key={index} className="swiper-slide h-full bg-white">
-                    <SwiperSlide className="bg-[#16161D] md:!h-[30rem] lg:!h-[30rem] p-8 xl:p-8 rounded-xl mx-2 border-solid border border-[#39393F]">
-                      <div className="h-full w-full flex flex-col justify-between">
-                        <p className="relative mt-4 text-gray-500">
-                          <span className="text-xl">&ldquo;</span>
-                          {testimonial.review.length <= 300
-                            ? testimonial.review
-                            : testimonial.review.slice(0, 300) + "..."}
-                          <span className="text-xl">&rdquo;</span>
-                        </p>
-                        <div className="flex flex-row gap-4 text-left items-start">
-                          <figcaption className="flex justify-center items-center space-x-3">
-                            <img
-                              className="w-12 h-12 rounded-full"
-                              src="/images/favicon.png"
-                              alt="profile picture"
-                            />
-                            <div className="space-y-0.5 font-medium dark:text-white text-left">
-                              <p className="font-medium font-SpaceGrotesk opacity-90 text-white text-xl">
-                                {testimonial.name}
-                              </p>
-                              <p className="font-medium font-SpaceGrotesk text-white opacity-70 text-md text-left">
-                                {testimonial.designation}
-                              </p>
-                            </div>
-                          </figcaption>
+              {TestimonialData.map((testimonial, index) => (
+                <SwiperSlide key={index} className="bg-[#16161D] md:!h-[30rem] lg:!h-[30rem] p-8 xl:p-8 rounded-xl mx-2 border-solid border border-[#39393F]">
+                  <div className="h-full w-full flex flex-col justify-between">
+                    <p className="relative mt-4 text-gray-500">
+                      <span className="text-xl">&ldquo;</span>
+                      {testimonial.review.length <= 300 ? testimonial.review : testimonial.review.slice(0, 300) + "..."}
+                      <span className="text-xl">&rdquo;</span>
+                    </p>
+                    <div className="flex flex-row gap-4 text-left items-start">
+                      <figcaption className="flex justify-center items-center space-x-3">
+                        <img
+                          className="w-12 h-12 rounded-full"
+                          src="/images/favicon.png"
+                          alt="profile picture"
+                        />
+                        <div className="space-y-0.5 font-medium dark:text-white text-left">
+                          <p className="font-medium font-SpaceGrotesk opacity-90 text-white text-xl">
+                            {testimonial.name}
+                          </p>
+                          <p className="font-medium font-SpaceGrotesk text-white opacity-70 text-md text-left">
+                            {testimonial.designation}
+                          </p>
                         </div>
-                      </div>
-                    </SwiperSlide>
+                      </figcaption>
+                    </div>
                   </div>
-                );
-              })}
+                </SwiperSlide>
+              ))}
             </Swiper>
+            <div className="swiper-button-next"></div>
+            <div className="swiper-button-prev"></div>
           </div>
         </div>
       ) : (
         <div id="sponsor-element" className="w-[90%]">
-          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 relative">
             <Swiper
               spaceBetween={25}
               breakpoints={{
@@ -103,13 +105,20 @@ export default function Slider(props: sliderProps) {
                 delay: 4000,
                 disableOnInteraction: false,
               }}
-              modules={[Autoplay]}
+              navigation={{
+                prevEl: ".swiper-button-prev",
+                nextEl: ".swiper-button-next",
+              }}
+              className="h-full"
             >
               {Sponsordata.map((sponsor, index) => {
                 return (
                   <div key={index} className="swiper-slide">
+
                     <SwiperSlide className="bg-[#16161D] md:!h-[35rem] p-8 xl:p-16 rounded-xl mx-2 border-solid border border-[#39393F]">
+
                       <div className="flex flex-col items-center justify-around space-y-7 ">
+
                         <p className="relative  mt-4 text-gray-500">
                           {sponsor.companydesc}
                         </p>
@@ -138,6 +147,8 @@ export default function Slider(props: sliderProps) {
                 );
               })}
             </Swiper>
+            <div className="swiper-button-next ssn"></div>
+            <div className="swiper-button-prev ssp"></div>
           </div>
         </div>
       )}
